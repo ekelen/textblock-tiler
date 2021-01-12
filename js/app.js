094c13061c44865ec6b13d3dd330b1ae6833294e
Elizabeth Kelen <erskelen@gmail.com>
2021-01-12 16:36:15 -0500

    add pseudo navigation(show/hide)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 const formEl = document.querySelector("#form__id");
 const textareaEl = formEl.querySelector("#textarea__id");
+const displayEl = document.querySelector("#display-container__id");
+
+const handleGoBack = () => {
+  formEl.style.display = "flex";
+  displayEl.style.display = "none";
+};
+
+displayEl
+  .querySelector("#back-btn__id")
+  .addEventListener("click", handleGoBack);
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const input = textareaEl.value;
 
-  //   formEl.style.display = "none";
-  console.log(`input:`, input);
+  formEl.style.display = "none";
+  displayEl.style.display = "flex";
 };
 
 const submitOnEnter = (event) => {
