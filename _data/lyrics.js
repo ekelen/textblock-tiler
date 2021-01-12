ad2f2a105b2d9fbb4673deb312a4c781370c7367
Elizabeth Kelen <erskelen@gmail.com>
2021-01-12 14:40:25 -0500

    add preset text data

---
+++ _data/lyrics.js
@@ -0,0 +1,25 @@
+module.exports = `
+ALL AMONG THE BARLEY
+
+Now is come September, the hunter's moon begun
+And through the wheaten stubble is heard the frequent gun
+The leaves are pale and yellow, and kindling into red
+And the ripe and bearded barley is hanging down its head
+
+All among the barley, who would not be blithe
+When the ripe and bearded barley is smiling on the scythe
+
+The spring is like a young man who does not know his mind
+The summer is a tyrant of most ungracious kind
+The autumn's like an old friend, who loves one all she can
+And she brings the bearded barley to glad the heart of man
+
+The wheat is like a rich man, it's sleek and well-to-do
+The oats are like a pack of girls, laughing and dancing too
+The rye is like a miser, it's sulky, lean, and small
+And the ripe and bearded barley is monarch of them all
+
+Now is come September, the hunter's moon begun
+And through the wheaten stubble is heard the frequent gun
+The leaves are pale and yellow, and kindling into red
+And the ripe and bearded barley is hanging down its head`;
---
+++ _data/recipe.js
@@ -0,0 +1,28 @@
+module.exports = `
+VEGAN LINZER COOKIES
+
+Ingredients
+
+    250 g (2.1 cups) flour
+    50 g (1.7 oz) ground almonds
+    200 g (0.8 cups) plant-based butter
+    100 g (0.5 cups) sugar
+    a pinch of salt
+    if needed: cold water
+    150 g (5.3 oz) raspberry jam (seedless)
+    50 g (0.5 cups) icing sugar
+
+Instructions
+
+    Add flour, ground almonds, sugar, and salt to a bowl and stir to combine.
+    Add diced cold plant-based butter on top. Start kneading with cold hands.
+    If needed, add cold water and knead until a smooth dough forms.
+    Wrap in plastic wrap and let rest in the fridge for at least 30 minutes.
+    Roll out the dough on a floured surface and cut out rounds. Use a smaller cutter to make a hole in the center of some rounds. In the end, you should get approx. Twenty-five rounds (the bottom of the cookies) and 25 rings (the top of the cookies). Place all of them on a baking tray covered with baking parchment.
+    Pre-heat the oven up to 180°C/356°F and bake cookies for 8-10 minutes. Allow to cool down for 10 minutes.
+    In the meantime heat the raspberry jam in a small pot.
+	Spoon 1/2 teaspoon of the raspberry jam into the center of the round. Top with a cookies ring and sprinkle the cookies with icing sugar.
+
+	Credit:
+	Zucker & Jagdwurst
+	https://www.zuckerjagdwurst.com/en/recipes/vegan-linzer-cookies`;
