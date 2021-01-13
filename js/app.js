a89d8816c5b22f645a10953c6a70f722e2de6595
Elizabeth Kelen <erskelen@gmail.com>
2021-01-13 11:40:12 -0500

    add collapse button to paragraphs

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@ const formEl = document.querySelector("#form__id");
 const textareaEl = formEl.querySelector("#textarea__id");
 const displayEl = document.querySelector("#display-container__id");
 
+/**
+ * Display area
+ */
+
 const handleGoBack = () => {
   formWrapperEl.style.display = "flex";
   displayEl.style.display = "none";
 
   const paragraphs = displayEl.querySelectorAll(".par-container__class");
   paragraphs.forEach((paragraph) => paragraph.remove());
-
   formWrapperEl.focus();
 };
 
@@ -18,7 +21,15 @@ displayEl
   .addEventListener("click", handleGoBack);
 
 /**
- * Form actions
+ * Paragraphs
+ */
+
+const toggleCollapse = (paragraphEl) => (_) => {
+  paragraphEl && paragraphEl.classList.toggle("collapsed");
+};
+
+/**
+ * Form
  */
 
 formWrapperEl.focus();
@@ -61,8 +72,19 @@ const handleSubmit = (event) => {
           ".par-text__class"
         ).innerHTML = paragraph;
 
-        paragraphDocFrag.id = `paragraph-${i}__id`;
+        const paragraphId = `paragraph-${i}__id`;
+
+        paragraphDocFrag.id = paragraphId;
         displayEl.appendChild(paragraphDocFrag);
+
+        // todo: collapse by default if block is very long
+
+        // Get the actual DOM node now that we have attached it
+        const paragraphEl = displayEl.querySelector(`#${paragraphId}`);
+
+        paragraphEl
+          .querySelector(".collapse-btn__class")
+          .addEventListener("click", toggleCollapse(paragraphEl));
       });
 };
 
